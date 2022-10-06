c5bf40d716e87eef6cf2ebd68b636d71d8f46201
JavierPoduje <javier.poduje@gmail.com>
2022-10-06 19:40:26 -0300

    feat: json_serialize

--- src/views/to_do/get.rs
+++ src/views/to_do/get.rs
@@ -1,9 +1,6 @@
-use crate::state::read_file;
-use actix_web::{web, Responder};
-use serde_json::value::Value;
-use serde_json::Map;
+use super::utils::return_state;
+use actix_web::Responder;
 
 pub async fn get() -> impl Responder {
-    let state: Map<String, Value> = read_file(String::from("./state.json"));
-    return web::Json(state);
+    return return_state();
 }
